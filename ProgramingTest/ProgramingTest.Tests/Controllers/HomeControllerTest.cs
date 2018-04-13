db6ff8725e52de4483bdb5e2df9e1159b5f741a7
a125760 <a125760@agl.com.au>
2018-04-14 05:04:59 +1000

    updates in testing project.

--- ProgramingTest/ProgramingTest.Tests/Controllers/HomeControllerTest.cs
+++ ProgramingTest/ProgramingTest.Tests/Controllers/HomeControllerTest.cs
@@ -8,6 +8,8 @@ using ProgramingTest;
 using ProgramingTest.Controllers;
 using PetOwnerServiceclass;
 using PetOwnersModel;
+using Moq;
+using Newtonsoft.Json;
 
 namespace ProgramingTest.Tests.Controllers
 {
@@ -18,6 +20,16 @@ namespace ProgramingTest.Tests.Controllers
         /// <summary>
         /// 
         /// </summary>
+        /// 
+        public virtual List<Owner> getlist()
+        {
+            throw new NotImplementedException();
+        }
+        
+        public virtual PetList sortjson(List<Owner> finalList)
+        {
+            throw new NotImplementedException();
+        }
         [TestMethod]
         public void Index()
         {
@@ -25,14 +37,50 @@ namespace ProgramingTest.Tests.Controllers
             {
 
                 // Arrange
-               // IPetOwnerList petOwnerList=;
+                PetOwnerList petOwnerList=new PetOwnerList();
                 HomeController controller = new HomeController(petOwnerList);
-
+                var mock = new Mock<HomeControllerTest>();
+                               
+                var People = "[{\"name\":\"Bob\",\"gender\":\"Male\",\"age\":23,\"pets\":[{\"name\":\"Garfield\",\"type\":\"Cat\"},{\"name\":\"Fido\",\"type\":\"Dog\"}]},{\"name\":\"Jennifer\",\"gender\":\"Female\",\"age\":18,\"pets\":[{\"name\":\"Garfield\",\"type\":\"Cat\"}]},{\"name\":\"Steve\",\"gender\":\"Male\",\"age\":45,\"pets\":null},{\"name\":\"Fred\",\"gender\":\"Male\",\"age\":40,\"pets\":[{\"name\":\"Tom\",\"type\":\"Cat\"},{\"name\":\"Max\",\"type\":\"Cat\"},{\"name\":\"Sam\",\"type\":\"Dog\"},{\"name\":\"Jim\",\"type\":\"Cat\"}]},{\"name\":\"Samantha\",\"gender\":\"Female\",\"age\":40,\"pets\":[{\"name\":\"Tabby\",\"type\":\"Cat\"}]},{\"name\":\"Alice\",\"gender\":\"Female\",\"age\":64,\"pets\":[{\"name\":\"Simba\",\"type\":\"Cat\"},{\"name\":\"Nemo\",\"type\":\"Fish\"}]}]";
+                
                 // Act
-                ViewResult result = controller.Index() as ViewResult;
+                var People_sort = JsonConvert.DeserializeObject<List<Owner>>(People);
+                mock.Setup(foo => foo.getlist()).Returns((People_sort));
+                var resultList = mock.Object.getlist();
+                CollectionAssert.AreEquivalent(People_sort, resultList);
+
+                People= "[{\"name\":\"Bob\",\"gender\":\"Male\",\"age\":23,\"pets\":[{\"name\":\"Garfield\",\"type\":\"Cat\"},{\"name\":\"Fido\",\"type\":\"Dog\"}]},{\"name\":\"Jennifer\",\"gender\":\"Female\",\"age\":18,\"pets\":[{\"name\":\"Garfield\",\"type\":\"Cat\"}]}]";
+                 People_sort = JsonConvert.DeserializeObject<List<Owner>>(People);
+                PetList petlist = new PetList();
+                List<OwnerGenderPet> logp = new List<OwnerGenderPet>();
+                   
+                OwnerGenderPet ogp = new OwnerGenderPet();
+              
+                ogp.OwnerGender = "Male";
+                ogp.PetName = "Garfield";
+                ogp.PetType = "Cat";
+                logp.Add(ogp);
+                              
+                ogp.OwnerGender = "Female";
+                ogp.PetName = "Garfield";
+                ogp.PetType = "Cat";
+
+                logp.Add(ogp);
+
+                petlist.GenderPets = logp;
+                List<PetList> pt = new List<PetList>();
+                pt.Add(petlist);
+                mock.Setup(foo => foo.sortjson(People_sort)).Returns((petlist));
+                List<PetList> pp = new List<PetList>();
+               
+                PetList resultList2 = mock.Object.sortjson(People_sort);
+                pp.Add(resultList2);
+                CollectionAssert.AreEquivalent(pt, pp);
+
+               // ActionResult result = controller.Index() as ActionResult;
 
                 // Assert
-                Assert.IsNotNull(result);
+               // Assert.IsNotNull(result);
             }
             catch (Exception ex) { }
         }
